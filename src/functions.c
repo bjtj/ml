899a81dae37a464702c319964e8a699f7613df4b
bjtj <bjtj10@gmail.com>
2017-11-28 14:06:09 +0900

    functions - updated

--- src/functions.c
+++ src/functions.c
@@ -6,12 +6,62 @@ double sigmoid(double x) {
 	return 1 / (1 + exp(-x));
 }
 
+typedef double (*func_ptr)(double x);
+
+typedef enum _func_type_e {
+	NONE,SIG,TANH,
+} func_type_e;
+
+typedef struct _func_t
+{
+    func_type_e type;
+	func_ptr func;
+} func_t;
+
+
+func_t functions[] = {
+	{SIG, sigmoid},
+	{TANH, tanh},
+	{NONE, NULL}
+};
+
+const char * to_func_type_str(func_type_e type) {
+	switch (type) {
+	case NONE:
+		return "none";
+	case SIG:
+		return "sigmoid";
+	case TANH:
+		return "tanh";
+	default:
+		break;
+	}
+	return "(UNKNOWN)";
+}
+
+void list(func_t * func, double from, double to, double step, FILE * out) {
+	double x;
+	printf("[%s]\n", to_func_type_str(func->type));
+	for (x = from; x <= to; x += step) {
+		fprintf(out, "%.4f %.4f\n", x, func->func(x));
+	}
+}
+
 int main(int argc, char *argv[])
 {
 	double x;
 
-	for (x = -3.0; x < 3.0; x += 0.1) {
-		printf("%f := %f\n", x, sigmoid(x));
+	func_t * elt = functions;
+
+	while (elt->type != NONE) {
+		FILE * fp = fopen(to_func_type_str(elt->type), "w");
+		if (fp == NULL) {
+			printf("Cannot open file\n");
+			exit(1);
+		}
+		list(elt, -3.0, 3.0, 0.1, fp);
+		fclose(fp);
+		elt++;
 	}
     
     return 0;
