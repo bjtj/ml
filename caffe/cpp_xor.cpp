f71503eee7a3989c52d6494c394b5707b7ce683f
bjtj <bjtj10@gmail.com>
2018-07-05 14:03:42 +0900

    indentation

--- caffe/cpp_xor.cpp
+++ caffe/cpp_xor.cpp
@@ -5,51 +5,51 @@
 int main(int argc, char *argv[])
 {
     float * data = new float[64*1*1*3*400];
-	float * label = new float[64*1*1*1*400];
+    float * label = new float[64*1*1*1*400];
 
-	for (int i = 0; i < 64 * 1 * 1 * 400; ++i) {
-		int a = rand() % 2;
-		int b = rand() % 2;
-		int c = a ^ b;
-		data[i * 2 + 0] = a;
-		data[i * 2 + 1] = b;
-		label[i] = c;
-	}
+    for (int i = 0; i < 64 * 1 * 1 * 400; ++i) {
+	int a = rand() % 2;
+	int b = rand() % 2;
+	int c = a ^ b;
+	data[i * 2 + 0] = a;
+	data[i * 2 + 1] = b;
+	label[i] = c;
+    }
 
-	caffe::SolverParameter solver_param;
-	caffe::ReadSolverParamsFromTextFileOrDie("./solver.prototxt", &solver_param);
+    caffe::SolverParameter solver_param;
+    caffe::ReadSolverParamsFromTextFileOrDie("./solver.prototxt", &solver_param);
 
-	boost::shared_ptr<caffe::Solver<float> > solver(caffe::SolverRegistry<float>::CreateSolver(solver_param));
-	caffe::MemoryDataLayer<float> * dataLayer_trainnet = (caffe::MemoryDataLayer<float> *) (solver->net()->layer_by_name("inputdata").get());
-	caffe::MemoryDataLayer<float> * dataLayer_testnet_ = (caffe::MemoryDataLayer<float> *) (solver->test_nets()[0]->layer_by_name("test_inputdata").get());
+    boost::shared_ptr<caffe::Solver<float> > solver(caffe::SolverRegistry<float>::CreateSolver(solver_param));
+    caffe::MemoryDataLayer<float> * dataLayer_trainnet = (caffe::MemoryDataLayer<float> *) (solver->net()->layer_by_name("inputdata").get());
+    caffe::MemoryDataLayer<float> * dataLayer_testnet_ = (caffe::MemoryDataLayer<float> *) (solver->test_nets()[0]->layer_by_name("test_inputdata").get());
 
-	float testab[] = {0, 0, 0, 1, 1, 0, 1, 1};
-	float testc[]=  {0, 1, 1, 0};
+    float testab[] = {0, 0, 0, 1, 1, 0, 1, 1};
+    float testc[]=  {0, 1, 1, 0};
 
-	dataLayer_testnet_->Reset(testab, testc, 4);
-	dataLayer_trainnet->Reset(data, label, 25600);
-	solver->Solve();
+    dataLayer_testnet_->Reset(testab, testc, 4);
+    dataLayer_trainnet->Reset(data, label, 25600);
+    solver->Solve();
 
-	boost::shared_ptr<caffe::Net<float> > testnet;
+    boost::shared_ptr<caffe::Net<float> > testnet;
 
-	testnet.reset(new caffe::Net<float>("./model.prototxt", caffe::TEST));
+    testnet.reset(new caffe::Net<float>("./model.prototxt", caffe::TEST));
 
-	testnet->ShareTrainedLayersWith(solver->net().get());
-	caffe::MemoryDataLayer<float> * dataLayer_testnet = (caffe::MemoryDataLayer<float> *) (testnet->layer_by_name("test_inputdata").get());
+    testnet->ShareTrainedLayersWith(solver->net().get());
+    caffe::MemoryDataLayer<float> * dataLayer_testnet = (caffe::MemoryDataLayer<float> *) (testnet->layer_by_name("test_inputdata").get());
 
-	dataLayer_testnet->Reset(testab, testc, 4);
-	testnet->Forward();
+    dataLayer_testnet->Reset(testab, testc, 4);
+    testnet->Forward();
 
-	boost::shared_ptr<caffe::Blob<float> > output_layer = testnet->blob_by_name("output");
+    boost::shared_ptr<caffe::Blob<float> > output_layer = testnet->blob_by_name("output");
 
-	const float * begin = output_layer->cpu_data();
-	const float * end = begin + 4;
+    const float * begin = output_layer->cpu_data();
+    const float * end = begin + 4;
 
-	std::vector<float> result(begin, end);
+    std::vector<float> result(begin, end);
 
-	for (size_t i = 0; i < result.size(); ++i) {
-		printf("input: %d xor %d, truth: %f result by nn %f\n", (int)testab[i * 2 + 0], (int)testab[i * 2 + 1], testc[i], result[i]);
-	}
+    for (size_t i = 0; i < result.size(); ++i) {
+	printf("input: %d xor %d, truth: %f result by nn %f\n", (int)testab[i * 2 + 0], (int)testab[i * 2 + 1], testc[i], result[i]);
+    }
 
     return 0;
 }
