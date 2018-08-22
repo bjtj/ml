ff1f384366acc266a5e2b40fe53b2fa813c75e1e
bjtj <bjtj10@gmail.com>
2018-08-22 10:17:00 +0900

    opencv version check

---
+++ opencv/version/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cout << CV_MAJOR_VERSION << "." << CV_MINOR_VERSION << "." << CV_VERSION_REVISION << endl;
+    return 0;
+}
